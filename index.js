c66be37a1a1a04fdc78dc90a64a81c072f011bde
Tyler Johnson <tyler@vintyge.com>
2014-05-11 15:19:40 -0600

    made it actually work with browserify

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ var PEG = require("pegjs"),
 	through = require("through"),
 	path = require("path");
 
-module.exports = function(options) {
+module.exports = function(file, options) {
+	if (path.extname(file) !== ".pegjs") return through();
+	
 	options = options || {};
 	options.output = "source";
-	options.extension = options.extension || ".pegjs";
 
-	return function(file) {
-		if (path.extname(file) !== options.extension) return through();
+	var src = "";
+	return through(write, end);
 
-		var src = "";
-		return through(write, end);
+	function write(chunk) {
+		src += chunk.toString("utf-8");
+	}
 
-		function write(chunk) {
-			src += chunk.toString("utf-8");
-		}
-
-		function end() {
-			var contents = "module.exports = " + PEG.buildParser(src, options);
-			this.emit("data", contents);
-			this.emit("end");
-		}
+	function end() {
+		var contents = "module.exports = " + PEG.buildParser(src, options);
+		this.emit("data", contents);
+		this.emit("end");
 	}
 }
