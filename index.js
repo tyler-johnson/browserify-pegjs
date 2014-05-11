963fc1713c13ac95ce99e8a8fd5a7d98435595fb
Tyler Johnson <tyler@vintyge.com>
2014-05-11 14:50:28 -0600

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+var PEG = require("pegjs"),
+	through = require("through"),
+	path = require("path");
+
+module.exports = function(options) {
+	options = options || {};
+	options.output = "source";
+	options.extension = options.extension || ".pegjs";
+
+	return function(file) {
+		if (path.extname(file) !== options.extension) return through();
+		console.log(file);
+
+		var src = "";
+		return through(write, end);
+
+		function write(chunk) {
+			src += chunk.toString("utf-8");
+		}
+
+		function end() {
+			var contents = "module.exports = " + PEG.buildParser(src, options);
+			this.emit("data", contents);
+			this.emit("end");
+		}
+	}
+}
